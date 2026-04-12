56eb573ac28a27b86df8dbc6cf36e4f133a7dd4c
RafaelNavon <raffi158@gmail.com>
2026-04-12 18:29:10 +0300

    fix CORS to use CLIENT_URL env variable

--- server/src/index.js
+++ server/src/index.js
@@ -17,7 +17,7 @@ const dashboardRoutes = require('./routes/dashboard');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
